62da7dde8f3d94e1ce44bbb5720549cc360264c5
Dreamfall <735369269@qq.com>
2019-07-14 12:13:52 +0800

    format headers

--- bilibili_blocker.js
+++ bilibili_blocker.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @name                 block bilibili danmaku and comments
-// @name:zh-CN           屏蔽bilibili弹幕和评论
-// @namespace            Dreamfall
-// @version              0.1
-// @description          block all the danmaku and comments of bilibili
-// @description:zh-CN    屏蔽全部bilibili弹幕和评论
-// @author               Dreamfall
-// @homepageURL          https://github.com/boeing888/bilibili_blocker
-// @downloadURL          https://github.com/boeing888/bilibili_blocker/raw/master/bilibili_blocker.js
-// @match                https://www.bilibili.com/video/*
-// @grant                none
+// @name         block bilibili danmaku and comments
+// @name:zh-CN   屏蔽bilibili弹幕和评论
+// @namespace    Dreamfall
+// @version      0.1
+// @description  block all the danmaku and comments of bilibili
+// @description:zh-CN 屏蔽全部bilibili弹幕和评论
+// @author       Dreamfall
+// @homepageURL  https://github.com/boeing888/bilibili_blocker
+// @downloadURL  https://github.com/boeing888/bilibili_blocker/raw/master/bilibili_blocker.js
+// @match        https://www.bilibili.com/video/*
+// @grant        none
 // ==/UserScript==
 
 (function () {
