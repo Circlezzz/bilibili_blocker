0b8593fc36919ee05126d3c236947e07b48078dd
Dreamfall <735369269@qq.com>
2019-07-14 12:05:19 +0800

    add downloadURL

--- bilibili_blocker.js
+++ bilibili_blocker.js
@@ -7,6 +7,7 @@
 // @description:zh-CN    屏蔽全部bilibili弹幕和评论
 // @author               Dreamfall
 // @homepageURL          https://github.com/boeing888/bilibili_blocker
+// @downloadURL          https://github.com/boeing888/bilibili_blocker/raw/master/bilibili_blocker.js
 // @match                https://www.bilibili.com/video/*
 // @grant                none
 // ==/UserScript==
