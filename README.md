39b19ccd41354d02adffa869f9a85701b0a7e47c
Dreamfall <735369269@qq.com>
2019-07-14 11:59:21 +0800

    Initial release

---
+++ README.md
@@ -0,0 +1,4 @@
+# 屏蔽全部bilibili弹幕和评论
+![TamperMonkey v4.5](https://img.shields.io/badge/TamperMonkey-v4.8-brightgreen.svg) ![Chrome x64 v60.4](https://img.shields.io/badge/Chrome%20x64-v73.0-brightgreen.svg)
+
+此脚本在打开bilibili视频页面时自动关闭弹幕并屏蔽所有评论。
---
+++ bilibili_blocker.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name                 block bilibili danmaku and comments
+// @name:zh-CN           屏蔽bilibili弹幕和评论
+// @namespace            Dreamfall
+// @version              0.1
+// @description          block all the danmaku and comments of bilibili
+// @description:zh-CN    屏蔽全部bilibili弹幕和评论
+// @author               Dreamfall
+// @homepageURL          https://github.com/boeing888/bilibili_blocker
+// @match                https://www.bilibili.com/video/*
+// @grant                none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    document.getElementById("comment").style.display = "none";
+    // define a new observer
+    var observer = new MutationObserver(function (mutations, observer) {
+        // look through all mutations that just occured
+        for (let i = 0; i < mutations.length; ++i) {
+            // look through all added nodes of this mutation
+            for (let j = 0; j < mutations[i].addedNodes.length; ++j) {
+                // was a child added with ID of 'bar'?
+                if (mutations[i].addedNodes[j].className == "bui-checkbox") {
+                    mutations[i].addedNodes[j].click();
+                }
+            }
+        }
+    });
+    observer.observe(document.getElementById("bilibiliPlayer"), { childList: true, subtree: true });
+})();
